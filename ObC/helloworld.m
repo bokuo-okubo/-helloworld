98b47afa1aa11335d9a5f2d1185f4f634819a8a2
bokuo-okubo <okubo.yohei@gmail.com>
2014-04-23 02:29:30 +0900

    added Objective-C code.

---
+++ ObC/helloworld.m
@@ -0,0 +1,21 @@
+#import <Foundation/NSObject.h>
+#import <stdio.h>
+
+@interface Greeter : NSObject
+- (void) sayHello;
+@end
+
+@implementation Greeter
+
+-(void) sayHello{
+  printf("Hello World. \n");
+}
+
+@end
+
+int main(void){
+  id greeter = [Greeter alloc];
+  [greeter sayHello];
+
+  return 0;
+}
